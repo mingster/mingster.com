fe50b0c3d1c219d5b0c4eaf855efdbb224d0f9a5
mingster <mingster@users.noreply.github.com>
2025-10-10 14:58:24 +0800

    WIP

--- web/src/app/dashboard/components/site-header.tsx
+++ web/src/app/dashboard/components/site-header.tsx
@@ -4,34 +4,13 @@ import DropdownUser from "@/components/auth/dropdown-user";
 import ThemeToggler from "@/components/theme-toggler";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { BackgroundImage } from "@/components/BackgroundImage";
 
 export function SiteHeader() {
 	return (
 		<header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
 			{/* background image */}
-			<div className="absolute inset-x-0 top-0 z-20 flex justify-center overflow-hidden pointer-events-none">
-				<div className="w-[108rem] flex-none flex justify-end">
-					<picture>
-						<source srcSet="/img/beams/docs@30.avif" type="image/avif" />
-						<img
-							src="/img/beams/docs@tinypng.png"
-							alt=""
-							className="w-[71.75rem] flex-none max-w-none dark:hidden"
-							decoding="async"
-						/>
-					</picture>
-					<picture>
-						<source srcSet="/img/beams/docs-dark@30.avif" type="image/avif" />
-						<img
-							src="/img/beams/docs-dark@tinypng.png"
-							alt=""
-							className="w-[90rem] flex-none max-w-none hidden dark:block"
-							decoding="async"
-						/>
-					</picture>
-				</div>
-			</div>
-
+			<BackgroundImage/>
 			<div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
 				<SidebarTrigger className="-ml-1" />
 				<Separator
